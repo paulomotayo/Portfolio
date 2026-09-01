680920c32b5e594f0abd35a36300bc0c9985e8c5
Paul <79833468+paulomotayo@users.noreply.github.com>
2026-09-01 11:57:36 +0100

    Add files via upload

---
+++ email-log-analyzer/report.py
@@ -0,0 +1,47 @@
+from operator import itemgetter
+from re import fullmatch
+
+
+def main():
+
+    with open("emails.txt", "r") as file:
+        ledger = analyze(file)  # analyze returns ledger: dict
+
+    if not ledger:
+        raise ValueError("Invalid .txt file")
+
+    top = max(ledger, key=ledger.get)
+
+    print(f"Top sender: {top} ({ledger[top]})\n")
+
+    # itemgetter(1) does the same as lambda s: s[1], just highly optimized
+    for key, value in sorted(ledger.items(), key=itemgetter(1), reverse=True):  
+        print(f"{key:<40}{value}")
+            
+
+
+def analyze(file):
+    ledger = {}
+
+    for address in file:  #iterate line by line, address could be \n too
+        address = validate(address.lower().strip())
+
+        if not address:
+            continue
+        
+        ledger[address] = ledger.get(address, 0) + 1
+
+    return ledger
+
+
+def validate(address):
+    pattern = r"(\w+(?:\.\w+)*@\w+(?:\.\w+)*\.com)"
+
+    if matches := fullmatch(pattern, address):
+        return matches.group(1)
+    
+    return None
+
+
+if __name__ == "__main__":
+    main()
